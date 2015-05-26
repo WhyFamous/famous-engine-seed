32bcf27d20f834ea12c3d6272aad1e070850b6a5
Tony Alves <tony@3alves.com>
2015-05-26 10:58:52 -0700

    gulp workflow

---
+++ gulp/config.js
@@ -0,0 +1,47 @@
+var dest = "./dist";
+var src = './src';
+var pub = './public';
+
+module.exports = {
+  browserSync: {
+    server: {
+      // Serve up our build folder
+      baseDir: dest
+    }
+  },
+  images: {
+    src: pub + "/images/**",
+    dest: dest + "/images"
+  },
+  ico: {
+    src: pub + "/favicon.ico",
+    dest: dest
+  },
+  markup: {
+    src: pub + "/**/*.html",
+    dest: dest + ""
+  },
+  styles: {
+    src: pub + "/styles/**",
+    dest: dest + "/styles"
+  },
+  browserify: {
+    // A separate bundle will be generated for each
+    // bundle config in the list below
+    bundleConfigs: [{
+      entries: src + '/index.js',
+      dest: dest,
+      outputName: 'bundle.js',
+      // list of externally available modules to exclude from the bundle
+      external: ['jquery']
+    }]
+  },
+  clean: {
+    dest: dest
+  },
+  production: {
+    cssSrc: dest + '/*.css',
+    jsSrc: dest + '/*.js',
+    dest: dest
+  }
+};
